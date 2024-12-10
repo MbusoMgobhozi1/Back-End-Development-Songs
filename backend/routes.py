b23ba19a38fa861c4af63e38d1c3ad67edc1e083
MbusoMgobhozi1 <mgobhozi360@icloud.com>
2024-12-10 15:58:51 -0500

    latest

--- backend/routes.py
+++ backend/routes.py
@@ -51,3 +51,58 @@ def parse_json(data):
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+
+@app.route("/health", methods=["GET"])
+def get_health():
+    return {"status": "ok"}, 200
+
+@app.route("/count", methods=["GET"])
+def get_count():
+    count_documents = len(songs_list)
+    return {"count": count_documents}, 200
+
+@app.route("/song", methods=["GET"])
+def songs():
+    list_of_songs = json_util.dumps(db.songs.find({}))
+    return {"songs": json.loads(list_of_songs)}
+
+@app.route("/song/<int:id>", methods=["GET"])
+def get_song_by_id(id):
+    song = json_util.dumps(db.songs.find_one({"id": id}))
+    if not song:
+        return {"message": "song with id not found"}, 404
+
+    return json.loads(song), 200
+
+@app.route("/song", methods=["POST"])
+def create_song():
+    data = request.get_json()
+
+    existing_song = db.songs.find_one({"id": data.get("id")})
+    if existing_song:
+        return {"Message": f"song with id {data.get('id')} already present"}, 302
+    
+    inserted_data = db.songs.insert_one(data)
+    return {"inserted_id": str(inserted_data.inserted_id)}
+
+@app.route("/song/<int:id>", methods=["PUT"])
+def update_song(id):
+    data = request.get_json()
+
+    existing_song = db.songs.find_one({"id": id})
+    if not existing_song:
+        return {"message": "song not found"}, 404
+
+    db.songs.update_one({"id": id}, {"$set": data})
+
+    updated_song_data = db.songs.find_one({"id": id})
+    return json.loads(json_util.dumps(updated_song_data)), 201
+
+@app.route("/song/<int:id>", methods=["DELETE"])
+def delete_song(id):
+    existing_song = db.songs.find_one({"id": id})
+    if not existing_song:
+        return {"message": "song not found"}, 404
+
+    db.songs.delete_one({"id": id})
+    return '' ,204
